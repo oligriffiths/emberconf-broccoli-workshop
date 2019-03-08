7d8f10227b404d0ab8bfe0b70334943fcd33065b
Oli Griffiths <oligriffiths@users.noreply.github.com>
2019-03-07 21:48:28 -0500

    Filter/merge

--- Brocfile.js
+++ Brocfile.js
@@ -1,4 +1,29 @@
+import funnel from 'broccoli-funnel';
+import merge from 'broccoli-merge-trees';
 
 export default () => {
-  return 'app';
+  const appRoot = 'src';
+
+  // Copy HTML file from app root to destination
+  const html = funnel(appRoot, {
+    files: ["index.html"],
+    annotation: "Index file",
+  });
+
+  // Copy JS files from app root to destination
+  const js = funnel(appRoot, {
+    include: ["**/*.js"],
+    destDir: 'assets/js',
+    annotation: "JS files",
+  });
+
+  // Copy CSS files from app root to destination
+  const css = funnel(appRoot, {
+    include: ["**/*.css"],
+    srcDir: 'styles',
+    destDir: 'assets/css',
+    annotation: "CSS files",
+  });
+
+  return merge([html, js, css]);
 }
