b675ce933142e0709f68d0aa04ec47411fb90e9c
Oli Griffiths <oligriffiths@users.noreply.github.com>
2019-03-09 14:34:44 -0500

    imports

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
-import foo from './foo';
-import { fooNamed } from "./foo";
+"use strict";
 
-console.log(foo);
+import foo from './foo';
+import { fooNamed } from './foo';
+import magic from 'magic-string';
+import { Bundle } from 'magic-string';
+import diff from 'arr-diff';
+
+console.log(foo, magic, Bundle, diff([1,2,3], [3,4,5]));
